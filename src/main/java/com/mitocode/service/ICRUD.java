799c26f7ee13372627a5e84da3cc7ee4ee066c60
Javier Marin <javiermarin@MacBook-Pro-de-Javier.local>
2023-01-02 09:20:51 -0600

    Se termino parcialmente la parte del Backend

--- src/main/java/com/mitocode/service/ICRUD.java
+++ src/main/java/com/mitocode/service/ICRUD.java
@@ -8,7 +8,7 @@ public interface ICRUD<T, ID> {
     T registrar(T t) throws Exception;
     T modificar(T t) throws Exception;
     List<T> listar() throws Exception;
-    T  listarPorId(ID id) throws Exception;
+    T listarPorId(ID id) throws Exception;
     void eliminar(ID id) throws Exception;
 
 }
